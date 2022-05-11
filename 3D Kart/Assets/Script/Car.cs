767bb0d3ea92c2268fc2b9a4a79e210f01e841ce
chaeryeon823 <chaechae823@naver.com>
2022-05-11 23:56:08 +0900

    1

---
+++ 3D Kart/Assets/Script/Car.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Car : MonoBehaviour
+{
+
+    public float carSpeed;   
+    public Transform target;
+    int nextTarget;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        target = GameManager.instance.target[nextTarget];
+        GetComponent<NavMeshAgent>().speed=carSpeed;
+        GetComponent<NavMeshAgent>().SetDestination(target.position);
+        StartCoroutine("AI_Move");
+    }
+    IEnumerator AI_Move()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(target.position);
+        while (true)
+        {
+            float dis=(target.position - transform.position).magnitude;
+            
+            if(dis<=1)
+            {
+                nextTarget += 1;
+                if (nextTarget >= GameManager.instance.target.Length)
+                    nextTarget = 0;
+                target=GameManager.instance.target[nextTarget];
+                GetComponent<NavMeshAgent>().SetDestination(target.position);
+            }
+            
+            
+            yield return null; 
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
